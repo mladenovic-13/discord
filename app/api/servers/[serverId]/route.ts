1ce5ca1d5df26a8565da3f363abb03c2a14ee900
Nikola Mladenović <nikolanik999@gmail.com>
2023-09-13 18:12:21 +0200

    feat: edit server modal

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,30 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+
+    const response = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log('[SERVERS_SERVERID_PATCH]', error);
+  }
+}
