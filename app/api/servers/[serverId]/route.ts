77db9530ed28712e2317d10e47a5aac1befcf44d
Nikola Mladenović <nikolanik999@gmail.com>
2023-09-19 13:58:22 +0200

    feat: leave & delete server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -28,3 +28,35 @@ export async function PATCH(
     console.log('[SERVERS_SERVERID_PATCH]', error);
   }
 }
+
+export async function DELETE(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      serverId: string;
+    };
+  }
+) {
+  try {
+    const profile = await currentProfile();
+    const { serverId } = params;
+
+    if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+    if (!serverId)
+      return new NextResponse('Server ID Missing', { status: 400 });
+
+    const server = await db.server.delete({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log('[SERVER_ID_DELETE]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -6,7 +6,9 @@ type ModalType =
   | 'invite'
   | 'editServer'
   | 'members'
-  | 'createChannel';
+  | 'createChannel'
+  | 'leaveServer'
+  | 'deleteServer';
 
 interface ModalData {
   server?: Server;
