477b6c8b0a8d855c3b377498e5a4f7ca45c07b3d
Nikola Mladenović <nikolanik999@gmail.com>
2023-09-19 23:50:00 +0200

    fix: create channel and conversations pages

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -28,8 +28,13 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const icon = roleIconMap[member.role];
 
+  const handleClick = () => {
+    router.push(`/servers/${server.id}/conversations/${member.id}`);
+  };
+
   return (
     <button
+      onClick={handleClick}
       className={cn(
         'group mb-1 flex w-full items-center gap-x-2 rounded-md p-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50',
         params.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700'
@@ -42,7 +47,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       <p
         className={cn(
           'text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300',
-          params.channelId === member.id &&
+          params.memberId === member.id &&
             'text-primary dark:text-zinc-200 dark:group-hover:text-white'
         )}
       >
