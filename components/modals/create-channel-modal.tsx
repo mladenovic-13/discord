e3ff8928772b46a3c2594f021b6f2f724cd008ad
Nikola Mladenović <nikolanik999@gmail.com>
2023-09-19 13:06:35 +0200

    feat: create channel

---
+++ components/modals/create-channel-modal.tsx
@@ -0,0 +1,167 @@
+'use client';
+
+import qs from 'query-string';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+import { useParams, useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/use-modal-store';
+import { ChannelType } from '@prisma/client';
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: 'Please enter a channel name.',
+    })
+    .refine((name) => name !== 'general', {
+      message: 'Channel name cannot be "general".',
+    }),
+  type: z.nativeEnum(ChannelType),
+});
+
+export const CreateChannelModal = () => {
+  const { type, isOpen, onClose } = useModal();
+  const router = useRouter();
+  const { serverId } = useParams();
+
+  const isModalOpen = isOpen && type === 'createChannel';
+
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      type: ChannelType.TEXT,
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: '/api/channels',
+        query: {
+          serverId: serverId,
+        },
+      });
+      await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className='overflow-hidden bg-white p-0 text-black'>
+        <DialogHeader className='px-6 pt-8'>
+          <DialogTitle className='text-center text-2xl font-bold'>
+            Create Channel
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+            <div className='space-y-8 px-6'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70'>
+                      Channel name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className='border-0 bg-zinc-300/50 text-black focus-visible:ring-0  focus-visible:ring-offset-0'
+                        placeholder='Enter channel name'
+                        autoFocus
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.name?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='type'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Channel Type</FormLabel>
+                    <FormControl>
+                      <Select
+                        disabled={isLoading}
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className='border-0 bg-zinc-300/50 capitalize text-black outline-none ring-offset-0 focus:ring-0 focus:ring-offset-0'>
+                          <SelectValue placeholder='Select a channel type' />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {Object.values(ChannelType).map((type) => (
+                            <SelectItem
+                              key={type}
+                              value={type}
+                              className='capitalize'
+                            >
+                              {type.toLowerCase()}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className='bg-gray-100 px-6 py-4'>
+              <Button variant='primary' disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
